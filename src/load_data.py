2df90f8a24980bafd6a7d9a9392f76c1974e337a
Dasilve <153503293+Dasilve@users.noreply.github.com>
2026-04-02 17:55:47 +0200

    load_data to test fullset

--- src/load_data.py
+++ src/load_data.py
@@ -1,2 +1,50 @@
 import os
 import pandas as pd
+
+def get_data_path(filename):
+
+    "Obtains the absolute row from data files."
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(base_path, '..', 'data', filename)
+
+
+
+def load_viral_dataset(filename):
+
+    path = get_data_path(filename)
+
+    print(f"Reading {filename}...")
+
+    
+
+    # paper says they don't have header, the label is in the last row (header=None)
+
+    df = pd.read_csv(path, header=None)
+
+    
+
+    X = df.iloc[:, :-1].values # Sequences
+
+    y = df.iloc[:, -1].values  # Lables (Last row)
+
+    
+
+    return X, y
+
+
+
+if __name__ == "__main__":
+
+    # Quick test to verify it's working
+
+    try:
+
+        X, y = load_viral_dataset('fullset_train')
+
+        print(f"Success. X: {X.shape}, y: {y.shape}")
+
+    except Exception as e:
+
+        print(f"Error: No files found on /data \n{e}")
